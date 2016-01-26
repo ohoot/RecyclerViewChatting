be846b2b203c1ea1dfda522be4205e70319c79f1
ohoot <ohoot@naver.com>
2016-01-26 21:05:16 +0900

    .

---
+++ app/src/main/java/friendslist/PersonData.java
@@ -0,0 +1,16 @@
+package friendslist;
+
+/**
+ * Created by Joo on 2016-01-26.
+ */
+public class PersonData {
+    String name;
+    int iconId;
+
+    public PersonData(){}
+
+    public PersonData(String name, int iconId) {
+        this.name = name;
+        this.iconId = iconId;
+    }
+}
